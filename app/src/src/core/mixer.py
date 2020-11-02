c20a3e3cd5c91650959167e4f17217befb6fa2bd
goal-eng <goooaldev@gmail.com>
2020-11-02 14:10:23 +0300

    Refactor imports

--- app/src/src/core/mixer.py
+++ app/src/src/core/mixer.py
@@ -26,14 +26,6 @@ def randstr():
     return ''.join(random.choices(string.ascii_lowercase, k=random.choice(range(1, 10))))
 
 
-def freeze(d):
-    if isinstance(d, dict):
-        return frozenset((key, freeze(value)) for key, value in d.items())
-    elif isinstance(d, list):
-        return tuple(freeze(value) for value in d)
-    return d
-
-
 class ApiMixer:
     """
     Given any dictionary in Swagger format, performs random substitutions of specific fields.
