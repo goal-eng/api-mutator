41346618e48160e58cb78410f6f16db818ed81df
Alex Goncharov <aleksandr-goncharov@reef.pl>
2021-07-01 17:54:19 +0300

    Add custom auth

--- app/src/src/core/models.py
+++ app/src/src/core/models.py
@@ -1,5 +1,29 @@
 from django.contrib.auth.models import User
 from django.db import models
+from functools import partial
+import secrets
+import string
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+def get_random_string(length: int) -> str:
+    return ''.join(
+        secrets.choice(string.ascii_uppercase + string.digits)
+        for _ in range(length)
+    )
+
+
+class ApiCredentials(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='api_credentials')
+    app_token = models.CharField(max_length=255, default=partial(get_random_string, length=16))
+    auth_token = models.CharField(max_length=255, default=partial(get_random_string, length=16))
+
+
+@receiver(post_save, sender=User)
+def generate_api_credentials(sender, instance, created, **kwargs):
+    if created:
+        ApiCredentials.objects.create(user=instance)
 
 
 class AccessAttemptFailure(models.Model):
