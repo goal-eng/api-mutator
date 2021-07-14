1f622ed36eafc0c1f3a322e9574c8e77bb73d5b9
Alex Goncharov <aleksandr-goncharov@reef.pl>
2021-07-14 15:41:29 +0300

    Set defaults for settings

--- app/src/src/settings.py
+++ app/src/src/settings.py
@@ -163,11 +163,11 @@ else:
 # how many failed attempts it is allowed to do before we block this user
 HUBSTAFF_MAX_FAILED_BEFORE_BLOCK = 3
 SWAGGER_FILE_PATH = Path(__file__).parent / 'core' / 'data' / 'hubstaff.v1.swagger.json'
-HUBSTAFF_APP_TOKEN = env('HUBSTAFF_APP_TOKEN')
-HUBSTAFF_AUTH_TOKEN = env('HUBSTAFF_AUTH_TOKEN', default=None)
-HUBSTAFF_USERNAME = env('HUBSTAFF_USERNAME')
-HUBSTAFF_PASSWORD = env('HUBSTAFF_PASSWORD')
-SUPPORT_EMAIL = env('SUPPORT_EMAIL')
+HUBSTAFF_APP_TOKEN = env('HUBSTAFF_APP_TOKEN', default='')
+HUBSTAFF_AUTH_TOKEN = env('HUBSTAFF_AUTH_TOKEN', default='')
+HUBSTAFF_USERNAME = env('HUBSTAFF_USERNAME', default='')
+HUBSTAFF_PASSWORD = env('HUBSTAFF_PASSWORD', default='')
+SUPPORT_EMAIL = env('SUPPORT_EMAIL', default='')
 
 if env('SENTRY_DSN', default=''):
     sentry_logging = LoggingIntegration(
