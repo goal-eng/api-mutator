c465bc0655b163a422b6cf07f823e1e238f607d3
Alex Goncharov <aleksandr-goncharov@reef.pl>
2021-07-14 23:07:45 +0300

    Remove confusing 'oauth' synonym

--- app/src/src/core/synonyms.py
+++ app/src/src/core/synonyms.py
@@ -1,49 +1,49 @@
 SYNONYMS = {
-  "auth": ["oauth", "login", "signin"],
-  "me": ["self", "myself"],
-  "users": ["user", "users", "employee", "employees", "account", "accounts", "member", "members", "staff", "people"],
-  "projects": ["task", "tasks", "subprojects", "subproject"],
-  "organizations": ["organization", "institution", "company", "companies", "groups"],
-  "integrations": ["connection", "connections", "setup", "setups"],
-  "last_activity": ["activity", "activities", "action", "actions", "last"],
-  "members": ["staff_member", "staff_members", "persons", "users"],
-  "links": ["integrations", "connectivity"],
-  "activities": ["activity", "actions", "action", "operations", "operation", "work", "working"],
-  "last_activities": ["activity", "actions", "action", "operations", "operation", "work", "working"],
-  "applications": ["application", "app", "apps"],
-  "urls": ["url", "link", "links"],
-  "screenshots": ["shots", "screens", "images"],
-  "notes": ["memos", "data"],
-  "tasks": ["todos", "task"],
-  "weekly": ["by_week", "week", "weeks", "seven_days"],
-  "my": ["own", "me", "myself", "i"],
-  "team": ["members", "team_members", "staff"],
-  "custom": ["specific", "advanced"],
-  "by_project": ["projects", "group_by_project", "project"],
-  "by_member": ["members", "member", "group_by_member"],
-  "by_date": ["date", "dates", "days", "day", "daily"],
-  "time_edit_logs": ["time_logs", "edit_logs"],
-  "team_payments": ["earnings", "money"],
-  "update_metadata": ["metadata_update", "set_metadata"],
-  "update_members": ["members_update", "set_members"],
-  "invites": ["invitations"],
+    "auth": ["authentication", "login", "signin"],
+    "me": ["self", "myself"],
+    "users": ["user", "users", "employee", "employees", "account", "accounts", "member", "members", "staff", "people"],
+    "projects": ["task", "tasks", "subprojects", "subproject"],
+    "organizations": ["organization", "institution", "company", "companies", "groups"],
+    "integrations": ["connection", "connections", "setup", "setups"],
+    "last_activity": ["activity", "activities", "action", "actions", "last"],
+    "members": ["staff_member", "staff_members", "persons", "users"],
+    "links": ["integrations", "connectivity"],
+    "activities": ["activity", "actions", "action", "operations", "operation", "work", "working"],
+    "last_activities": ["activity", "actions", "action", "operations", "operation", "work", "working"],
+    "applications": ["application", "app", "apps"],
+    "urls": ["url", "link", "links"],
+    "screenshots": ["shots", "screens", "images"],
+    "notes": ["memos", "data"],
+    "tasks": ["todos", "task"],
+    "weekly": ["by_week", "week", "weeks", "seven_days"],
+    "my": ["own", "me", "myself", "i"],
+    "team": ["members", "team_members", "staff"],
+    "custom": ["specific", "advanced"],
+    "by_project": ["projects", "group_by_project", "project"],
+    "by_member": ["members", "member", "group_by_member"],
+    "by_date": ["date", "dates", "days", "day", "daily"],
+    "time_edit_logs": ["time_logs", "edit_logs"],
+    "team_payments": ["earnings", "money"],
+    "update_metadata": ["metadata_update", "set_metadata"],
+    "update_members": ["members_update", "set_members"],
+    "invites": ["invitations"],
 
-  # "application_activities"
-  # "daily"
-  # "url_activities"
-  # "clients"
-  # "locations"
-  # "client_invoices"
-  # "team_invoices"
-  # "time_off_policies"
-  # "time_off_requests"
-  # "attendance_schedules"
-  # "holidays"
-  # "job_sites"
-  # "timesheets"
-  # "attendance_shifts"
-  # "budget_histories"
-  # "integration_links"
-  # "user_links"
-  # "project_links"
+    # "application_activities"
+    # "daily"
+    # "url_activities"
+    # "clients"
+    # "locations"
+    # "client_invoices"
+    # "team_invoices"
+    # "time_off_policies"
+    # "time_off_requests"
+    # "attendance_schedules"
+    # "holidays"
+    # "job_sites"
+    # "timesheets"
+    # "attendance_shifts"
+    # "budget_histories"
+    # "integration_links"
+    # "user_links"
+    # "project_links"
 }
