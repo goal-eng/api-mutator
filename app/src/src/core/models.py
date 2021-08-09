9e8f2b1093a29880a0e48d5d656cf35670524fe9
goal-eng <goooaldev@gmail.com>
2021-08-09 19:40:08 +0300

    Store password along with ApiCredentials

--- app/src/src/core/models.py
+++ app/src/src/core/models.py
@@ -7,6 +7,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+PASSWORD_LENGTH = 8
+
+
 def get_random_string(length: int) -> str:
     return ''.join(
         secrets.choice(string.ascii_uppercase + string.digits)
@@ -14,10 +17,20 @@ def get_random_string(length: int) -> str:
     )
 
 
+def password_default() -> str:
+    return get_random_string(PASSWORD_LENGTH)
+
+
 class ApiCredentials(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='api_credentials')
+    password = models.CharField(max_length=PASSWORD_LENGTH, default=password_default)
     app_token = models.CharField(max_length=255, default=partial(get_random_string, length=16))
     auth_token = models.CharField(max_length=255, default=partial(get_random_string, length=16))
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.user.set_password(self.password)
+        self.user.save()
 
 
 @receiver(post_save, sender=User)
